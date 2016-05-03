5b1e8ed84917982327f06a598e703e6e4a53236c
hsmang <hsmang@naver.com>
2016-05-04 00:10:45 +0900

    22222

--- app/src/main/java/com/example/han/myapplication/MainActivity.java
+++ app/src/main/java/com/example/han/myapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDr awerToggle(
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
